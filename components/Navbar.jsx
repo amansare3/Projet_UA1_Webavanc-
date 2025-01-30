196176cc867f2d99e2521008f153d1b579f7b735
Ayoub Jaziri <2017rezz@gmail.com>
2025-01-30 15:33:51 -0500

    Update Navbar Responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,32 @@
-import './Navbar.css'
+"use client"; 
 
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
+import "./Navbar.css"; 
 
 const Navbar = () => {
-    return ( 
-        <header className='header'>
-            <a href= "/" className='logo'> <img src="/logo.png" alt="" height={100} width={100}/></a>
+  const [isOpen, setIsOpen] = useState(false);
 
-            <nav className='navbar'>
-        <Link href='/'>Home</Link>
-        <Link href='/personaltraining'>Personal training</Link>
-        <Link href='/'>Group classes</Link>
-        <Link href='/'>Membership</Link>
-        <Link href='/'>Contact</Link>
-        </nav>
-            </header>
+  return (
+    <header className="header">
+      <Link href="/" className="logo">
+        <img src="/logo.png" alt="Logo" height={80} width={80} />
+      </Link>
 
-    )
-}
+      
+      <div className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </div>
 
-export default Navbar
+      <nav className={`navbar ${isOpen ? "active" : ""}`}>
+        <Link href="/">Home</Link>
+        <Link href="/personaltraining">Personal Training</Link>
+        <Link href="/">Group Classes</Link>
+        <Link href="/">Membership</Link>
+        <Link href="/">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
